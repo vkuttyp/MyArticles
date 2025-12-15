dcd4a452d8086ed057a7068b03aa3c158bfd344e
Veeran Puthumkara <vkuttyp@gmail.com>
2025-12-15 18:36:41 +0300

    content config update

--- content.config.ts
+++ content.config.ts
@@ -8,15 +8,15 @@ export default defineContentConfig({
                 source: "articles/**/*.md",
                 type: "page",
                 schema: z.object({
+                    title: z.string(),
                     author: z.string(),
                     author_avatar: z.string(),
                     author_description: z.string(),
                     date: z.date(),
                     description: z.string(),
-                    rawbody: z.string(),
-                    tags: z.optional(z.array(z.string())),
+                    tags: z.optional(z.array(z.string())),                    
                     thumbnail: z.string(),
-                    title: z.string(),
+                    rawbody: z.string(),
                 }),
             })
         ),
